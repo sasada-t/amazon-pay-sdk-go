c60949e78dc888407bdeaa31a112572aae5d671a
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2024-07-11 11:18:03 +0900

    feat: add tools

---
+++ tools/tools.go
@@ -0,0 +1,9 @@
+//go:build tools
+
+package tool
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint" //nolint
+	_ "golang.org/x/tools/cmd/goimports"                    //nolint
+	_ "mvdan.cc/gofumpt"                                    //nolint
+)
