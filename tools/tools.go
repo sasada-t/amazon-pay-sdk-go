5beda92935c9202b8e75bcaf247de7f941887dcf
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2024-07-11 19:53:43 +0900

    feat: add air

--- tools/tools.go
+++ tools/tools.go
@@ -3,6 +3,7 @@
 package tool
 
 import (
+	_ "github.com/air-verse/air"                            //nolint
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint" //nolint
 	_ "golang.org/x/tools/cmd/goimports"                    //nolint
 	_ "mvdan.cc/gofumpt"                                    //nolint
